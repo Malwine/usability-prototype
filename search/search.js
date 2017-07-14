65dec9a5816f40ba53bb32b4b2eec54d68f6aeab
Malwine <malwine@mailbox.org>
2017-07-14 23:11:11 +0200

    Mehr happy path

--- search/search.js
+++ search/search.js
@@ -17,45 +17,53 @@ var courses = [
 $('#autocomplete').autocomplete({
     lookup: courses,
     onSelect: function (suggestion) {
-        var thehtml =
-            '<div class="groups-headline more-space">Rechnungswesen</div>'+
-            '<div class="all-groups">'+
-            '<div class="inner">'+
+      var strVar="";
+      strVar += "<div class=\"groups-headline more-space\">Rechnungswesen<\/div>";
+      strVar += "          <div class=\"all-groups\">";
+      strVar += "            <div class=\"inner\">";
+      strVar += "              <a class=\"box\" href=\"..\/group1.html\">";
+      strVar += "                <div class=\"upper\">";
+      strVar += "                  <div class=\"group-title\">ReWe Chaos<\/div>";
+      strVar += "                  <div class=\"attending\">";
+      strVar += "                    <img src=\"..\/images\/person-icon-white.png\"> <span";
+      strVar += "                      class=\"number-attendees\">9<\/span>";
+      strVar += "                  <\/div>";
+      strVar += "                <\/div>";
+      strVar += "                <div class=\"below\">";
+      strVar += "                  <strong class=\"time\">Mo, 17.Juli, 18 Uhr. Jede zweite Woche.<\/strong>";
+      strVar += "                  <div class=\"description\">Bitte gut vorbereiten.<\/div>";
+      strVar += "                <\/div>";
+      strVar += "              <\/a>";
+      strVar += "              <a class=\"box\" href=\"..\/janas-group.html\">";
+      strVar += "                <div class=\"upper\">";
+      strVar += "                  <div class=\"group-title\">ReWe für Alle<\/div>";
+      strVar += "                  <div class=\"attending\">";
+      strVar += "                    <img src=\"..\/images\/person-icon-white.png\"> <span";
+      strVar += "                      class=\"number-attendees\">3<\/span>";
+      strVar += "                  <\/div>";
+      strVar += "                <\/div>";
+      strVar += "                <div class=\"below\">";
+      strVar += "                  <strong class=\"time\">Do, 27.Juli, 15 Uhr. Jede Woche.<\/strong>";
+      strVar += "                  <div class=\"description\">Alle willkommen!<\/div>";
+      strVar += "                <\/div>";
+      strVar += "              <\/a>";
+      strVar += "              <a class=\"box\" href=\"..\/group1.html\">";
+      strVar += "                <div class=\"upper\">";
+      strVar += "                  <div class=\"group-title\">ReWe Profis<\/div>";
+      strVar += "                  <div class=\"attending\">";
+      strVar += "                    <img src=\"..\/images\/person-icon-white.png\"> <span";
+      strVar += "                      class=\"number-attendees\">7<\/span>";
+      strVar += "                  <\/div>";
+      strVar += "                <\/div>";
+      strVar += "                <div class=\"below\">";
+      strVar += "                  <strong class=\"time\">Mi, 01.August, 15 Uhr.<\/strong>";
+      strVar += "                  <div class=\"description\">Wir treffen uns *nur* in den";
+      strVar += "                    Semesterferien.<\/div>";
+      strVar += "                <\/div>";
+      strVar += "              <\/a>";
+      strVar += "            <\/div>";
+      strVar += "          <\/div>";
 
-            '<a class="box" href="../group1.html">'+
-            '<div class="upper">'+
-            '<div class="group-title">ReWe für Alle</div>'+
-        '<div class="attending">'+
-          '<img src="../images/person-icon-white.png">'+
-            '<span class="number-attendees">9</span>'+
-            '</div></div><div class="below"><strong class="time">Mo, 17.Juli, 18 Uhr. Jede 2. Woche.</strong>'+
-        '<div class="description">Wir treffen uns auch in den Semesterferien.</div></div></a>'+
-        '<a class="box" href="../group1.html">'+
-            '<div class="upper">'+
-            '<div class="group-title">ReWe Chaotinnen</div>'+
-        '<div class="attending">'+
-            '<img src="../images/person-icon-white.png">'+
-            '<span class="number-attendees">8</span>'+
-            '</div>'+
-            '</div>'+
-            '<div class="below">'+
-            '<strong class="time">Di, 15.Juli, 15 Uhr.</strong>'+
-        '<div class="description">Wir treffen uns auch in den Semesterferien.</div>'+
-        '</div>'+
-        '</a>'+
-        '<a class="box" href="../group1.html">'+
-            '<div class="upper">'+
-            '<div class="group-title">ReWe Profis</div>'+
-        '<div class="attending">'+
-            '<img src="../images/person-icon-white.png">'+
-            '<span class="number-attendees">7</span>'+
-            '</div>'+
-            '</div>'+
-            '<div class="below">'+
-            '<strong class="time">Mi, 16.Juli, 15 Uhr. Alle 2 Wochen.</strong>'+
-        '<div class="description">Wir treffen uns auch in den Semesterferien.</div>'+
-        '</div>'+
-        '</a>';
-        $('#outputcontent').html(thehtml);
+        $('#outputcontent').html(strVar);
     }
 });
